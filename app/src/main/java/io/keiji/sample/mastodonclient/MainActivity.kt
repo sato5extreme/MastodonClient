2df3f11598df5633da0cd6502b5abd2a0a93b0e8
sato5extreme <sato5extreme@gmail.com>
2020-06-23 13:52:03 +0900

    initial commit

---
+++ app/src/main/java/io/keiji/sample/mastodonclient/MainActivity.kt
@@ -0,0 +1,21 @@
+package io.keiji.sample.mastodonclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import io.keiji.sample.mastodonclient.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+           this,
+           R.layout.activity_main
+       )
+        binding.textview.text = "Hello DataBinding"
+    }
+}
